4af481f54fc52aebf8c6d99bbffc01a23850f58d
Paula Virginia Burgos <paulavburgos@gmail.com>
2022-03-31 17:42:37 -0300

    estilos terminados

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path')
 const port = 3030
 
-app.use(express.static('public'))
+app.use(express.static(path.resolve(__dirname, "public")))
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
 
